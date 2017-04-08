3e5c22d3467e6b0010033f775f798f8180c58758
James Mitchell <mitchell.thehumantornado@gmail.com>
2017-04-08 13:32:44 -0500

    changing to fix deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost/createaculture');
 
 require('./config/routes.js')(app, express);
-//require('./config/middleware.js')(app, express);
+require('./config/middleware.js')(app, express);
 
 var port = process.env.PORT || 4000;
 app.listen(port, function () {
